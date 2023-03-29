112a1067830fef0e4bd58ff03e67ad11cca15f49
Antopasquettin90 <119460334+Antopasquettin90@users.noreply.github.com>
2023-03-29 16:53:20 -0300

    primer commit

---
+++ Carrito/js/products.js
@@ -0,0 +1,33 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Sabado 1/04/23",
+    precio: 1200,
+    img:"/imagenes/quijote-nuevo.jpeg",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "Sabado 8/04/23",
+    precio: 900,
+    img:
+      "/imagenes/quijote-neon.jpeg",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "Sabado 15/04/23",
+    precio: 1100,
+    img:
+      "/imagenes/quijote-canva.png",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "Sabado 22/04/23",
+    precio: 800,
+    img:
+      "/imagenes/quijote-azul.jpeg",
+    cantidad: 1,
+  },
+];
